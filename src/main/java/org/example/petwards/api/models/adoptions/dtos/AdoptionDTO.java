0569b51c72aceb29bfe0b5894b38088ad8dbfc07
Thibault Delaunois <thibaultdelaunois@gmail.com>
2025-03-04 16:47:54 +0100

    Modif AdoptionDTO

--- src/main/java/org/example/petwards/api/models/adoptions/dtos/AdoptionDTO.java
+++ src/main/java/org/example/petwards/api/models/adoptions/dtos/AdoptionDTO.java
@@ -1,13 +1,16 @@
 package org.example.petwards.api.models.adoptions.dtos;
 
+import org.example.petwards.api.models.beasts.dtos.BeastDTO;
 import org.example.petwards.dl.entities.Adoption;
 import org.example.petwards.dl.enums.AdoptionStatus;
 
 public record AdoptionDTO(
         Long id,
-        AdoptionStatus adoptionStatus
+        AdoptionStatus adoptionStatus,
+        BeastDTO beastDTO
 ) {
     public static AdoptionDTO fromAdoption(Adoption adoption){
-        return new AdoptionDTO(adoption.getId(), adoption.getStatus());
+        BeastDTO beastDTO = BeastDTO.fromBeast(adoption.getBeast());
+        return new AdoptionDTO(adoption.getId(), adoption.getStatus(), beastDTO);
     }
 }
